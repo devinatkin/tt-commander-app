28217f572fb8e628bc33cde0499e13c1ff5b613e
Devin <devinatkin@gmail.com>
2024-06-19 09:24:27 -0600

    feat: move test project and test all to TTBoardDevice

--- src/ttcontrol/TTBoardDevice.ts
+++ src/ttcontrol/TTBoardDevice.ts
@@ -23,8 +23,8 @@ export const frequencyTable = [
   { title: '12 MHz', value: '12000000' },
   { title: '10 MHz', value: '10000000' },
   { title: '1 MHz', value: '1000000' },
-  { title: '50 kHz', value: '50000' },
-  { title: '10 kHz', value: '10000' },
+  { title: '50 KHz', value: '50000' },
+  { title: '10 KHz', value: '10000' },
 ];
 
 export interface ILogEntry {
@@ -129,6 +129,16 @@ export class TTBoardDevice extends EventTarget {
 
   async resetProject() {
     await this.sendCommand('reset_project()');
+  }
+
+  async testProjects(): Promise<string> {
+    await this.sendCommand(`tt.shuttle.test_all()\r`);
+    return 'Test all command sent.';
+  }
+
+  async testProject(project: Project): Promise<string> {
+    await this.sendCommand(`tt.shuttle.${project.macro}.run_test()\r`);
+    return `Test project ${project.macro} command sent.`;
   }
 
   async manualClock() {
