3abefc3494a45d25a4c8cb3c841eddc641285677
Devin <devinatkin@gmail.com>
2024-07-10 17:37:28 -0600

    Additional git functions

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/rest";
+import { Octokit } from '@octokit/rest';
 
 // Import required modules
 import * as dotenv from 'dotenv';
@@ -69,7 +69,7 @@ export async function extractForksFromURL(githubURL: string): Promise<string[] |
     }
 
     return forkURLs;
-  } catch (error: any) {
+  } catch (error: unknown) {
     console.error('Error fetching forks:', error);
     return null;
   }
@@ -77,13 +77,13 @@ export async function extractForksFromURL(githubURL: string): Promise<string[] |
 
 /**
  * Fetches test code from a given GitHub repository.
- * 
+ *
  * @param {string} repoURL - The URL of the GitHub repository.
  * @param {string} path - The path within the repo to fetch data from.
  * @param {string} branch - The branch to fetch data from.
  * @returns {Promise<string[] | null>} - An array of files in the specified path.
  */
-export async function fetchTestCode(repoURL: string, path = "/test/commander", branch = "main") {
+export async function fetchTestCode(repoURL: string, path = '/test/commander', branch = 'main') {
   // Verify the API key exists
   const apiKey = process.env.GH_API_KEY;
   if (!apiKey) {
@@ -104,7 +104,7 @@ export async function fetchTestCode(repoURL: string, path = "/test/commander", b
       owner,
       repo,
       path,
-      ref: branch
+      ref: branch,
     });
 
     // Check if the returned data is a single file or a directory listing
@@ -112,17 +112,91 @@ export async function fetchTestCode(repoURL: string, path = "/test/commander", b
       // Return the list of file paths in the directory
       return data;
     } else {
-      console.error("Path points to a file, not a directory.");
+      console.error('Path points to a file, not a directory.');
       return null;
     }
-
-  } catch (error: any) {
+  } catch (error: unknown) {
     if (error.status === 404) {
       // As not everyone will implement a test file, this needs to be informational, not an error
-      console.info("Not Found");
+      console.info('Not Found');
     } else {
-      console.error("Error Retrieving:", error);
+      console.error('Error Retrieving:', error);
     }
+    return null;
+  }
+}
+
+/**
+ * Fetches the list of artifacts available for a given GitHub repository.
+ *
+ * @param {string} repoURL - The URL of the GitHub repository.
+ * @returns {Promise<string[] | null>} - An array of artifacts available for the repository.
+ */
+export async function fetchArtifacts(
+  repoURL: string,
+  rejectExpired: boolean = false,
+): Promise<Array<{ name: string; archive_download_url: string; created_at: string }> | null> {
+  // Verify the API key exists
+  const apiKey = process.env.GH_API_KEY;
+  if (!apiKey) {
+    throw new Error('GH_API_KEY is not defined in the environment variables.');
+  }
+
+  // Initialize Octokit
+  const octokit = new Octokit({ auth: apiKey });
+
+  const repository = extractRepoFromURL(repoURL);
+  if (!repository) {
+    throw new Error('Invalid GitHub URL.');
+  }
+  const [owner, repo] = repository.split('/');
+
+  try {
+    const { data } = await octokit.actions.listArtifactsForRepo({
+      owner,
+      repo,
+    });
+
+    const artifacts = data.artifacts
+      .filter((artifact: { expired: boolean }) => !rejectExpired || !artifact.expired)
+      .map(
+        (artifact: { name: string; archive_download_url: string; created_at: string | null }) => ({
+          name: artifact.name,
+          archive_download_url: artifact.archive_download_url,
+          created_at: artifact.created_at ?? '',
+        }),
+      );
+
+    return artifacts;
+  } catch (error: unknown) {
+    console.error('Error fetching artifacts:', error);
+    return null;
+  }
+}
+
+/**
+ * Returns the latest artifact with a specific name from the given array of artifacts.
+ *
+ * @param {Array<{ name: string, archive_download_url: string, created_at: string }>} artifacts - The array of artifacts.
+ * @param {string} name - The name of the artifact to search for.
+ * @returns {string|null} - The download URL of the latest artifact with the specified name, or null if not found.
+ */
+export function getLatestArtifact(
+  artifacts: Array<{ name: string; archive_download_url: string; created_at: string }>,
+  name: string,
+): string | null {
+  // Filter the artifacts by name
+  const filteredArtifacts = artifacts.filter((artifact) => artifact.name === name);
+
+  // Sort the filtered artifacts by creation date in descending order
+  const sortedArtifacts = filteredArtifacts.sort(
+    (a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+  );
+
+  // Return the download URL of the latest artifact, if found
+  if (sortedArtifacts.length > 0) {
+    return sortedArtifacts[0].archive_download_url;
+  } else {
     return null;
   }
 }
