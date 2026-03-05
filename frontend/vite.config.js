bef6a3bfddc393ca96eaf08a12aa17add01c5e72
Devin Atkin <devinatkin@gmail.com>
2026-03-05 12:12:20 -0700

    Let Github Pages Handle the HTTPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import suidPlugin from '@suid/vite-plugin';
-import child from 'child_process';
 import path from 'path';
-import fs from 'fs';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { execSync } from 'child_process';
 
 
 let commitHash = 'N/A';
@@ -14,26 +13,12 @@ try {
   console.warn('Not a git repository, using default commit hash.');
 }
 
-// Paths to Certificate Files
-const keyPath = path.resolve(__dirname, 'key.pem');
-const certPath = path.resolve(__dirname, 'cert.pem');
-
-// Check if the certificate files exist, if not, set https to false
-const httpsOptions =
-  fs.existsSync(keyPath) && fs.existsSync(certPath)
-    ? {
-        key: fs.readFileSync(keyPath),
-        cert: fs.readFileSync(certPath),
-      }
-    : false;
-
 export default defineConfig({
   plugins: [suidPlugin(), solidPlugin()],
   base: process.env.BASE_URL || '/',
 
   server: {
     open: false,
-    https: httpsOptions,
     port: process.env.PORT || 5173,
     proxy: {
       '/api': {
